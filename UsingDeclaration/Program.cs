909d98d310801e86a710573aa6e3ca4b043d89f7
siddheshmalvankar <siddheshmalvankar@gmail.com>
2020-11-30 16:37:06 +0530

    Add project files.

---
+++ UsingDeclaration/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UsingDeclaration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo Using Declaration in C# 8.0");
+            List<string> lstLines = new List<string>();
+            lstLines.Add("1 is One");
+            lstLines.Add("2 is Two");
+            lstLines.Add("3 is Three");
+            WriteToFile(lstLines);
+            Console.ReadKey();
+        }
+
+        static void WriteToFile(IEnumerable<string> lstLines)
+        {
+            //Using Declaration 
+            using var file = new StreamWriter("G:\\Demo\\DemoUsingDeclaration.txt");
+            
+                foreach (var line in lstLines)
+                {
+                    file.WriteLine(line + Environment.NewLine);
+                }
+            
+            
+        }
+    }
+}
